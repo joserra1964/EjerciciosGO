fd739e767e8cd0689a28cd1c95c694e0a9ba947c
Jose Ramon <j.parada.vigo@gmail.com>
2022-05-26 12:29:45 +0200

    accesos publico y privado

--- src/miPaquete/miPaquete.go
+++ src/miPaquete/miPaquete.go
@@ -1,7 +1,18 @@
 package mipaquete
 
+import "fmt"
+
 // CarPublic Car con acceso público
 type CarPublic struct {
 	Marca string
 	Año   int
+}
+
+type carPrivado struct {
+	marca string
+	año   int
+}
+
+func ImprimeMensaje(texto string) {
+	fmt.Println(texto)
 }
--- src/modificadoresAccesos.go
+++ src/modificadoresAccesos.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	mipaquete "EjerciciosGO/src/miPaquete"
+	pk "EjerciciosGO/src/miPaquete"
 	"fmt"
 )
 
 // Si la primera letra es mayúscula es público y si es minúscula, es de ámbito privado
+// solucionados problemas de errores en GOROOT y GOPATH con
+// go mod init
+// go mod tidy
 
 func main() {
-	var miCoche mipaquete.CarPublic
+	var miCoche pk.CarPublic
 	miCoche.Marca = "Ferrari"
+	miCoche.Año = 2020
 	fmt.Println(miCoche)
+
+	// ImprimeMensaje
+	pk.ImprimeMensaje("hola")
+
 }
